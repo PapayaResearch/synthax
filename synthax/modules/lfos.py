Fixing issues with JAX's broadcast

synthax/modules/lfos.py
@@ -142,7 +142,7 @@ class LFO(ControlRateModule):
         # If no modulation, then return a view of the frequency of this
         # LFO expanded to the control buffer size
         if mod_signal is None:
-            return jnp.broadcast_to(frequency, (-1, self.control_buffer_size))
+            return jnp.broadcast_to(frequency, (frequency.shape[0], self.control_buffer_size))
 
         modulation = jnp.expand_dims(
             self.parameters["mod_depth"].from_0to1(), axis=1

synthax/modules/oscillators.py
@@ -107,7 +107,7 @@ class VCO(SynthModule):
         # number of values
         if mod_signal is None:
             control_hz = midi_to_hz(midi_f0)
-            return jnp.broadcast_to(control_hz, (-1, self.buffer_size))
+            return jnp.broadcast_to(control_hz, (control_hz.shape[0], self.buffer_size))
 
         # If there is modulation, then add that to the fundamental,
         # clamp to a range [0.0, 127.0], then return in frequency Hz.