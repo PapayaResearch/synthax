7c7b55c24e6966584b8d057174ef89a9816e1d52
Manuel Cherep <manuelcherep@gmail.com>
2023-06-22 19:08:49 -0400

    Increase package version to v0.1.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 from setuptools import setup, find_packages
 
 setup(name="synthax",
-      version="0.1.1",
+      version="0.1.2",
       url="https://github.com/PapayaResearch/synthax",
       author="Manuel Cherep, Nikhil Singh",
       author_email="mcherep@mit.edu, nsingh1@mit.edu",
