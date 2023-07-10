8604f5d31b9b4c50ac06066b43c3082afce6c389
Manuel Cherep <manuelcherep@gmail.com>
2023-07-10 14:45:11 -0400

    New version v0.2.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 from setuptools import setup, find_packages
 
 setup(name="synthax",
-      version="0.1.2",
+      version="0.2.0",
       url="https://github.com/PapayaResearch/synthax",
       author="Manuel Cherep, Nikhil Singh",
       author_email="mcherep@mit.edu, nsingh1@mit.edu",
